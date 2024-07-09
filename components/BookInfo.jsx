f15af4b1635090d5449912a4b527afefe200ce3b
CronoCode120 <ctaverafernandez@gmail.com>
2024-07-09 18:54:17 +0200

    created and styled BookInfo component

---
+++ components/BookInfo.jsx
@@ -0,0 +1,13 @@
+import { Container, InfoText, Title } from './styles/BookInfo.js'
+
+const BookInfo = () => {
+  return (
+    <Container>
+      <Title>Title</Title>
+      <InfoText>Author</InfoText>
+      <InfoText>Genre</InfoText>
+    </Container>
+  )
+}
+
+export default BookInfo
