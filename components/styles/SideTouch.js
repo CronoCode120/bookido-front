1a2c1673db6185b188c7d539de5866a54efd9ee6
CronoCode120 <ctaverafernandez@gmail.com>
2024-07-10 13:54:51 +0200

    adjusted SideTouch component inside BookCard dimensions

--- components/styles/SideTouch.js
+++ components/styles/SideTouch.js
@@ -1,18 +1,16 @@
-import { Dimensions } from 'react-native'
 import styled from 'styled-components/native'
 
 export const TouchWrapper = styled.View`
   position: absolute;
-  bottom: 0px;
+  top: 0px;
   left: 0px;
   z-index: 99;
-  width: ${Dimensions.get('window').width}px;
-  height: ${Dimensions.get('window').height}px;
+  width: 100%;
+  height: 100%;
   flex-direction: row;
   justify-content: space-between;
 `
 
 export const SidePressable = styled.Pressable`
-  border: 1px solid black;
   width: 80px;
 `
