a4af718124cabd7ab6b5e2f12db2afa416111044
CronoCode120 <ctaverafernandez@gmail.com>
2024-07-10 13:55:45 +0200

    split SideTouch into a separate component

---
+++ components/SideTouch.jsx
@@ -0,0 +1,12 @@
+import { SidePressable, TouchWrapper } from './styles/SideTouch.js'
+
+const SideTouch = ({ handleLeft, handleRight }) => {
+  return (
+    <TouchWrapper>
+      <SidePressable onPress={handleLeft} />
+      <SidePressable onPress={handleRight} />
+    </TouchWrapper>
+  )
+}
+
+export default SideTouch
