7c7a55cd5765424e563f6a14760ae3e152e77957
CronoCode120 <ctaverafernandez@gmail.com>
2024-07-10 18:26:01 +0200

    used custom function for height instead of Dimensions object

--- components/styles/CoverView.js
+++ components/styles/CoverView.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components/native'
-import { Dimensions } from 'react-native'
+import { getHeightPercent } from '../../utils/dimensions.js'
 
 export const Cover = styled.Image`
-  height: ${(Dimensions.get('window').height / 100) * 50}px;
   width: auto;
+  height: ${getHeightPercent(45)}px;
   max-height: 450px;
   aspect-ratio: 1/1.5;
   border-radius: 20px;
