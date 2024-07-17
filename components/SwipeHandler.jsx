58576ed1903075fe71ca97fcf44489c08c2b1230
CronoCode120 <ctaverafernandez@gmail.com>
2024-07-18 00:03:01 +0200

    animated element rotation and position reset
    removed 'runOnJs' to improve performance

--- components/SwipeHandler.jsx
+++ components/SwipeHandler.jsx
@@ -1,3 +1,4 @@
+import { useWindowDimensions } from 'react-native'
 import {
   Gesture,
   GestureDetector,
@@ -5,21 +6,38 @@ import {
 } from 'react-native-gesture-handler'
 import Animated, {
   useSharedValue,
-  useAnimatedStyle
+  useDerivedValue,
+  useAnimatedStyle,
+  interpolate,
+  withTiming,
+  Easing
 } from 'react-native-reanimated'
 
 const SwipeHandler = ({ children }) => {
+  const { width } = useWindowDimensions()
+
   const translationX = useSharedValue(0)
   const translationY = useSharedValue(0)
   const prevTranslationX = useSharedValue(0)
   const prevTranslationY = useSharedValue(0)
 
+  const MAX_ROTATION = 60
+  const rotate = useDerivedValue(
+    () =>
+      interpolate(translationX.value, [0, width * 2], [0, MAX_ROTATION]) + 'deg'
+  )
+
   const animatedStyles = useAnimatedStyle(() => ({
     transform: [
       { translateX: translationX.value },
-      { translateY: translationY.value }
+      { translateY: translationY.value },
+      { rotate: rotate.value }
     ]
   }))
+
+  const resetAnim = withTiming(0, {
+    easing: Easing.out(Easing.back(2))
+  })
 
   const pan = Gesture.Pan()
     .minDistance(1)
@@ -31,7 +49,10 @@ const SwipeHandler = ({ children }) => {
       translationX.value = prevTranslationX.value + event.translationX
       translationY.value = prevTranslationY.value + event.translationY
     })
-    .runOnJS(true)
+    .onEnd(() => {
+      translationX.value = resetAnim
+      translationY.value = resetAnim
+    })
 
   return (
     <GestureHandlerRootView
