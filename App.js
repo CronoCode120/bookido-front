8a891850316e729a2f9410c170f2339325f9d91d
CronoCode120 <ctaverafernandez@gmail.com>
2024-07-17 18:58:44 +0200

    fetched data from API and rendered through a FlatList component

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import BookCard from "./components/BookCard.jsx";
+import { useState, useEffect } from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import BookCard from './components/BookCard.jsx'
 
 export default function App() {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    getBooks()
+  }, [])
+
+  const getBooks = async () => {
+    try {
+      const res = await fetch('https://bookido-back.onrender.com/books')
+      const data = await res.json()
+      console.log(data)
+
+      setBooks(data.books)
+    } catch (error) {
+      console.log(error)
+      return <Text>Ha ocurrido un error</Text>
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <BookCard />
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      <FlatList
+        data={books}
+        renderItem={({ item }) => <BookCard item={item} />}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
