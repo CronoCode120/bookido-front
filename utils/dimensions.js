9384d5e444ae5fe15ed4620422261408b01ac825
CronoCode120 <ctaverafernandez@gmail.com>
2024-07-17 14:35:53 +0200

    refactored 'getDimensions' to make it more reusable and brief

--- utils/dimensions.js
+++ utils/dimensions.js
@@ -1,9 +1,6 @@
 import { Dimensions } from 'react-native'
 
-export const getDimensions = () => {
-  const { width, height } = Dimensions.get('window')
-  return { width, height }
-}
+export const getDimensions = () => Dimensions.get('window')
 
 export const getHeightPercent = percent =>
   (getDimensions().height / 100) * percent
