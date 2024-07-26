c4ac06dd0d261d7284a85e9a09610130eab63390
CronoCode120 <ctaverafernandez@gmail.com>
2024-07-26 13:55:58 +0200

    added authors information getter function

--- components/BookInfo.jsx
+++ components/BookInfo.jsx
@@ -1,10 +1,18 @@
 import { Container, InfoText, Title } from './styles/BookInfo.js'
 
 const BookInfo = ({ title, author, publisher }) => {
+  const getAuthors = () => {
+    const length = author.length
+    if (length >= 3) return `${author[0]} y ${length - 1} más`
+    if (length === 2) return author.join(' y ')
+    if (length === 1) return author[0]
+    return 'Sin información del autor'
+  }
+
   return (
     <Container>
       <Title>{title}</Title>
-      <InfoText>{author}</InfoText>
+      <InfoText>{getAuthors()}</InfoText>
       <InfoText>{publisher}</InfoText>
     </Container>
   )
