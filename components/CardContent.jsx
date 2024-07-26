2ba620804b29128d9df37cdf112c059801507c4e
CronoCode120 <ctaverafernandez@gmail.com>
2024-07-26 13:57:18 +0200

    passed publisher and isbn properly to CoverView component

--- components/CardContent.jsx
+++ components/CardContent.jsx
@@ -3,13 +3,20 @@ import DetailView from './DetailView.jsx'
 import { ContentWrapper } from './styles/BookCard.js'
 
 const CardContent = ({ pageNum, book }) => {
+  const {
+    title,
+    author,
+    publisher,
+    isbn: [isbn]
+  } = book
   return (
     <ContentWrapper>
       {(pageNum === 0 && (
         <CoverView
-          title={book.title}
-          author={book.author}
-          publisher={book.publisher}
+          title={title}
+          author={author}
+          publisher={publisher[0]}
+          isbn={isbn}
         />
       )) ||
         (pageNum === 1 && <DetailView />)}
