8cf75adcbeaff501a4d3e194d78a35a59c8fdda5
CronoCode120 <ctaverafernandez@gmail.com>
2024-08-09 10:43:46 +0200

    added dynamic styles to handle synopsis text overflow

--- components/DetailView.jsx
+++ components/DetailView.jsx
@@ -1,12 +1,32 @@
-import { Text, View } from 'react-native'
-import { SynopsisTitle, Wrapper } from './styles/DetailView.js'
+import { Pressable, Text, View } from 'react-native'
+import {
+  Synopsis,
+  SynopsisContainer,
+  SynopsisTitle,
+  Wrapper
+} from './styles/DetailView.js'
+import { useState } from 'react'
 
 const DetailView = ({ synopsis, tags }) => {
+  const [lines, setLines] = useState(12)
+  const LINE_HEIGHT = 20
+
+  const calcLines = e => {
+    const { height } = e.nativeEvent.layout
+    const lines = Math.round(height / LINE_HEIGHT)
+    setLines(lines)
+  }
+
   return (
     <>
       <Wrapper>
         <SynopsisTitle>Sinopsis</SynopsisTitle>
-        <Text>{synopsis}</Text>
+        <SynopsisContainer onLayout={calcLines}>
+          <Synopsis numberOfLines={lines}>{synopsis}</Synopsis>
+        </SynopsisContainer>
+        <Pressable>
+          <Text>Ver más</Text>
+        </Pressable>
       </Wrapper>
       <View>
         <Text>Tags</Text>
--- components/styles/DetailView.js
+++ components/styles/DetailView.js
@@ -2,9 +2,21 @@ import styled from 'styled-components/native'
 
 export const Wrapper = styled.View`
   align-self: stretch;
+  height: 60%;
+  overflow: hidden;
 `
 
 export const SynopsisTitle = styled.Text`
   font-weight: 600;
   font-size: 20px;
+  margin-bottom: 6px;
+`
+
+export const SynopsisContainer = styled.View`
+  flex: 1;
+`
+
+export const Synopsis = styled.Text`
+  font-size: 16px;
+  overflow: hidden;
 `
