2e0a8904ae9ba90ac3d1eeac5ca40c42aa07b4ea
CronoCode120 <ctaverafernandez@gmail.com>
2024-08-11 15:37:07 +0200

    handled receiving undefined author

--- components/BookInfo.jsx
+++ components/BookInfo.jsx
@@ -2,7 +2,7 @@ import { Container, InfoText, Title } from './styles/BookInfo.js'
 
 const BookInfo = ({ title, author, publisher }) => {
   const getAuthors = () => {
-    const length = author.length
+    const length = author?.length
     if (length >= 3) return `${author[0]} y ${length - 1} más`
     if (length === 2) return author.join(' y ')
     if (length === 1) return author[0]
