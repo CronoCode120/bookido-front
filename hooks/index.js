20c1c59b2e1d0a2703e6af01bce1c945a767fa0c
CronoCode120 <ctaverafernandez@gmail.com>
2024-08-13 11:50:11 +0200

    exported all hooks from index.js file

---
+++ hooks/index.js
@@ -0,0 +1,4 @@
+export { default as useBooks } from './useBooks.js'
+export { default as useBookSynopsis } from './useBookSynopsis.js'
+export { default as useSwipe } from './useSwipe.js'
+export { default as useSwipeAnimation } from './useSwipeAnimation.js'
