eec484476db10502a95141753fef44267d4147e1
CronoCode120 <ctaverafernandez@gmail.com>
2024-08-13 17:20:44 +0200

    relocated 'getAuthors' function to 'utils'

--- components/swipe/BookInfo.jsx
+++ components/swipe/BookInfo.jsx
@@ -1,18 +1,11 @@
+import getAuthors from '../../utils/getAuthors.js'
 import { Container, InfoText, Title } from '../styles/BookInfo.js'
 
 const BookInfo = ({ title, author, publisher }) => {
-  const getAuthors = () => {
-    const length = author?.length
-    if (length >= 3) return `${author[0]} y ${length - 1} más`
-    if (length === 2) return author.join(' y ')
-    if (length === 1) return author[0]
-    return 'Sin información del autor'
-  }
-
   return (
     <Container>
       <Title>{title}</Title>
-      <InfoText>{getAuthors()}</InfoText>
+      <InfoText>{getAuthors(author)}</InfoText>
       <InfoText>{publisher}</InfoText>
     </Container>
   )
