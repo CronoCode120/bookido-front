33edf4595c21abba0a2db7789e97caf8814b4af2
CronoCode120 <ctaverafernandez@gmail.com>
2024-08-21 11:15:33 +0200

    exported all icons through index file

---
+++ components/icons/index.js
@@ -0,0 +1,4 @@
+export { default as AddIcon } from './AddIcon.jsx'
+export { default as LikeIcon } from './LikeIcon.jsx'
+export { default as DislikeIcon } from './DislikeIcon.jsx'
+export { default as HeartIcon } from './HeartIcon.jsx'
