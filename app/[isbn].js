d805e54d704a209957548fa77a3676db9226e457
CronoCode120 <ctaverafernandez@gmail.com>
2024-08-26 16:04:29 +0200

    added new 'review' page

---
+++ app/[isbn].js
@@ -0,0 +1,10 @@
+import { useLocalSearchParams } from 'expo-router'
+import Review from '../components/nightstand/Review.jsx'
+
+const ReviewPage = () => {
+  const { isbn } = useLocalSearchParams()
+
+  return <Review isbn={isbn} />
+}
+
+export default ReviewPage
