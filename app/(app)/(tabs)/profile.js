105052e354cf44d867acf645150407700c8d0bd0
CronoCode120 <ctaverafernandez@gmail.com>
2024-09-04 13:24:09 +0200

    created profile page with log out functionality

--- app/(app)/(tabs)/profile.js
+++ app/(app)/(tabs)/profile.js
@@ -1,5 +1,25 @@
-import { Text } from 'react-native'
+import Screen from '../../../components/Screen.jsx'
+import { useSession } from '../../../context/SessionProvider'
+import { Text, View, Pressable } from 'react-native'
 
-const Profile = () => <Text>Perfil</Text>
+const Profile = () => {
+  const { signOut } = useSession()
+
+  return (
+    <Screen
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <Text>Perfil</Text>
+      <View>
+        <Pressable onPress={signOut}>
+          <Text>Cerrar sesión</Text>
+        </Pressable>
+      </View>
+    </Screen>
+  )
+}
 
 export default Profile
