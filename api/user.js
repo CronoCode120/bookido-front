cb4ccd3cbee3d78a8dfed68481439b0fcc02ce95
CronoCode120 <ctaverafernandez@gmail.com>
2024-09-04 13:00:43 +0200

    created user api functions to log in and register

---
+++ api/user.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const apiUrl = axios.create({
+  baseURL: 'https://bookido-back.onrender.com/users/'
+})
+
+export const logIn = async ({ email, password }) =>
+  (await apiUrl.post('/login', { email, password })).data
+
+export const register = async ({ username, email, password }) =>
+  (await apiUrl.post('/', { username, email, password })).data
