3faecacd684d3320ece67bd78b21832deee414e1
CronoCode120 <ctaverafernandez@gmail.com>
2024-09-05 12:50:59 +0200

    added swiping functionality to interact with back end

--- components/swipe/BookSwipe.jsx
+++ components/swipe/BookSwipe.jsx
@@ -1,27 +1,39 @@
 import { useState } from 'react'
-import { useSwipe } from '../../hooks'
+import { useSwipe, useBooks } from '../../hooks'
 import BookList from './BookList.jsx'
 import SwipeButtons from './SwipeButtons.jsx'
 
+import { useSession } from '../../context/SessionProvider.js'
+import { addToTable, discardBook } from '../../api/books.js'
+
 const BookSwipe = () => {
   const [curIdx, setCurIdx] = useState(0)
+  const page = Math.floor((curIdx + 3) / 20) + 1
+
+  const [books] = useBooks(page)
+  const { session } = useSession()
+
   const [btnDisabled, setBtnDisabled] = useState(false)
+  const curIsbn = books[curIdx]?.isbn[0]
 
   const toggleBtnCooldown = () => {
     setBtnDisabled(true)
     setTimeout(() => setBtnDisabled(false), 1500)
   }
 
-  const onSwipeLeft = () => {
-    console.log('left')
+  const handleSwipe = () => {
     setCurIdx(prevIdx => prevIdx + 1)
     toggleBtnCooldown()
   }
 
+  const onSwipeLeft = () => {
+    discardBook(curIsbn, session)
+    handleSwipe()
+  }
+
   const onSwipeRight = () => {
-    console.log('right')
-    setCurIdx(prevIdx => prevIdx + 1)
-    toggleBtnCooldown()
+    addToTable(curIsbn, session)
+    handleSwipe()
   }
 
   const swipes = [
@@ -44,6 +56,7 @@ const BookSwipe = () => {
   return (
     <>
       <BookList
+        books={books}
         curIdx={curIdx}
         swipeInstances={swipes}
         setCurSwipe={setCurSwipe}
