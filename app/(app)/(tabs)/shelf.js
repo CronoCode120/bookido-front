2cb9981754772422bc8655694a23bcd6cca92e78
CronoCode120 <ctaverafernandez@gmail.com>
2024-09-05 13:34:31 +0200

    implemented shelf logic by fetching user data

--- app/(app)/(tabs)/shelf.js
+++ app/(app)/(tabs)/shelf.js
@@ -1,20 +1,37 @@
+import { useEffect, useState } from 'react'
+import { Text } from 'react-native'
 import BookItem from '../../../components/BookItem.jsx'
 import BookList from '../../../components/BookList.jsx'
+import Screen from '../../../components/Screen.jsx'
+import { getBooksInShelf } from '../../../api/books.js'
 
-import books from '../../../books.json'
+import { useSession } from '../../../context/SessionProvider.js'
 
 const Shelf = () => {
+  const { session } = useSession()
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    getBooksInShelf(session).then(data => setBooks(data.shelf))
+  }, [])
+
   const handlePress = book => {
     console.log('delete ' + book.isbn[0])
   }
 
   return (
-    <BookList
-      books={books}
-      renderBook={book => (
-        <BookItem book={book} action='delete' onPress={handlePress} />
+    <Screen>
+      {books?.length ? (
+        <BookList
+          books={books}
+          renderBook={isbn => (
+            <BookItem isbn={isbn} action='delete' onPress={handlePress} />
+          )}
+        />
+      ) : (
+        <Text>Tu estantería está vacía.</Text>
       )}
-    />
+    </Screen>
   )
 }
 
