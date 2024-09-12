ec016fe6566405ad7aede7d64d3356512c9702c8
CronoCode120 <ctaverafernandez@gmail.com>
2024-09-12 19:01:22 +0200

    modified LinkButton content and styles to match design

--- components/nightstand/DrawerButtons.jsx
+++ components/nightstand/DrawerButtons.jsx
@@ -1,10 +1,26 @@
+import theme from '../../theme.js'
 import Button, { LinkButton } from '../Button.jsx'
-import { Wrapper } from './styles/DrawerButtons.js'
+import { AddIcon } from '../icons'
+import {
+  ContentWrapper,
+  LabelWrapper,
+  LinkLabel,
+  LinkTitle,
+  Wrapper
+} from './styles/DrawerButtons.js'
 
 const DrawerButtons = ({ isbn }) => {
   return (
     <Wrapper>
-      <LinkButton href={`/review/${isbn}`}>Marcar como leído</LinkButton>
+      <LinkButton href={`/review/${isbn}`}>
+        <ContentWrapper>
+          <LabelWrapper>
+            <LinkTitle>Añadir</LinkTitle>
+            <LinkLabel>Reseña y valoración</LinkLabel>
+          </LabelWrapper>
+          <AddIcon color={theme.colors.WHITE} />
+        </ContentWrapper>
+      </LinkButton>
       <Button type='tertiary'>Caja de donaciones</Button>
     </Wrapper>
   )
--- components/nightstand/styles/DrawerButtons.js
+++ components/nightstand/styles/DrawerButtons.js
@@ -3,3 +3,27 @@ import styled from 'styled-components/native'
 export const Wrapper = styled.View`
   gap: 20px;
 `
+
+export const ContentWrapper = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+`
+
+export const LabelWrapper = styled.View`
+  gap: 4px;
+`
+
+export const LinkTitle = styled.Text`
+  line-height: 24px;
+  font-size: 16px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors.WHITE};
+`
+
+export const LinkLabel = styled.Text`
+  line-height: 24px;
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.WHITE};
+`
