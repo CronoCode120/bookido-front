a8b68b9a770dcb53f3fcceb97f69d38b980a5160
CronoCode120 <ctaverafernandez@gmail.com>
2024-09-18 17:27:21 +0200

    added color gradient and blur to background

--- components/BookItem.jsx
+++ components/BookItem.jsx
@@ -5,6 +5,7 @@ import { AddIcon, CrossIcon } from './icons/index.js'
 import { Cover } from './styles/Cover.js'
 import {
   ActionButton,
+  Blur,
   DetailWrapper,
   Header,
   Heading,
@@ -14,6 +15,7 @@ import {
   Wrapper
 } from './styles/BookItem.js'
 import { Shadow } from 'react-native-shadow-2'
+import { BlurView } from 'expo-blur'
 
 const BookItem = ({ isbn, onPress, action }) => {
   const [book] = useBookISBN(isbn, ['title', 'author', 'publisher'])
@@ -30,24 +32,26 @@ const BookItem = ({ isbn, onPress, action }) => {
       style={{ borderRadius: 12 }}
       containerStyle={{ marginHorizontal: 14 }}
     >
-      <Wrapper>
-        <Header>
-          <Heading>
-            <Title>{title}</Title>
-            <InfoText>{authorInfo}</InfoText>
-          </Heading>
-          <ActionButton
-            onPress={() => onPress({ ...book, isbn, cover: coverUri })}
-          >
-            {action === 'add' && <AddIcon />}
-            {action === 'delete' && <CrossIcon />}
-          </ActionButton>
-        </Header>
-        <DetailWrapper>
-          <Cover src={coverUri} $width='20%' />
-          <Synopsis numberOfLines={4}>{synopsis}</Synopsis>
-        </DetailWrapper>
-      </Wrapper>
+      <Blur>
+        <Wrapper>
+          <Header>
+            <Heading>
+              <Title>{title}</Title>
+              <InfoText>{authorInfo}</InfoText>
+            </Heading>
+            <ActionButton
+              onPress={() => onPress({ ...book, isbn, cover: coverUri })}
+            >
+              {action === 'add' && <AddIcon />}
+              {action === 'delete' && <CrossIcon />}
+            </ActionButton>
+          </Header>
+          <DetailWrapper>
+            <Cover src={coverUri} $width='20%' />
+            <Synopsis numberOfLines={4}>{synopsis}</Synopsis>
+          </DetailWrapper>
+        </Wrapper>
+      </Blur>
     </Shadow>
   )
 }
--- components/styles/BookItem.js
+++ components/styles/BookItem.js
@@ -1,11 +1,21 @@
 import styled from 'styled-components/native'
+import { LinearGradient } from 'expo-linear-gradient'
+import { BlurView } from 'expo-blur'
 
-export const Wrapper = styled.View`
+export const Blur = styled(BlurView).attrs({
+  experimentalBlurMethod: 'dimezisBlurView',
+  intensity: 40
+})`
+  border-radius: 12px;
+  overflow: hidden;
+`
+
+export const Wrapper = styled(LinearGradient).attrs({
+  colors: ['#F1F5F980', '#F1F5F933']
+})`
   width: 100%;
   padding: 14px;
   gap: 14px;
-  border-radius: 12px;
-  background: #e2e8f0;
 `
 
 export const Header = styled.View`
