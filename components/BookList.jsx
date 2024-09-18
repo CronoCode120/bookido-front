049274c2a9fd96d74d65ef6e6d9a41a41db48d38
CronoCode120 <ctaverafernandez@gmail.com>
2024-09-18 17:14:48 +0200

    added ImageBackground to BookList component

--- components/BookList.jsx
+++ components/BookList.jsx
@@ -1,19 +1,23 @@
 import { FlatList } from 'react-native'
+import { ImgBackground } from './styles/BookList.js'
+import standImg from '../assets/mesita.png'
 
 const BookList = ({ books, renderBook }) => {
   return (
-    <FlatList
-      data={books}
-      renderItem={({ item }) => renderBook(item)}
-      keyExtractor={item => item}
-      contentContainerStyle={{
-        alignSelf: 'stretch',
-        gap: 30,
-        backgroundColor: 'white',
-        paddingVertical: 8,
-        overflow: 'auto'
-      }}
-    />
+    <ImgBackground source={standImg}>
+      <FlatList
+        data={books}
+        renderItem={({ item }) => renderBook(item)}
+        keyExtractor={item => item}
+        contentContainerStyle={{
+          alignSelf: 'stretch',
+          gap: 30,
+          background: 'transparent',
+          paddingVertical: 8,
+          overflow: 'auto'
+        }}
+      />
+    </ImgBackground>
   )
 }
 
