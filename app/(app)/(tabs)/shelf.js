b688ed00c50310b7e40d5e3c07213b0dae005af5
CronoCode120 <ctaverafernandez@gmail.com>
2024-09-19 18:58:20 +0200

    modified shelf page

--- app/(app)/(tabs)/shelf.js
+++ app/(app)/(tabs)/shelf.js
@@ -1,18 +1,18 @@
 import { useEffect, useState } from 'react'
-import { Text } from 'react-native'
 import BookItem from '../../../components/BookItem.jsx'
 import BookList from '../../../components/BookList.jsx'
-import Screen from '../../../components/Screen.jsx'
 import { getBooksInShelf } from '../../../api/books.js'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 import { useSession } from '../../../context/SessionProvider.js'
+import shelfImg from '../../../assets/estanteria.png'
 
 const Shelf = () => {
   const { session } = useSession()
   const [books, setBooks] = useState([])
 
   useEffect(() => {
-    getBooksInShelf(session).then(data => setBooks(data.shelf))
+    getBooksInShelf(session).then(data => setBooks(data.shelve))
   }, [])
 
   const handlePress = book => {
@@ -20,18 +20,16 @@ const Shelf = () => {
   }
 
   return (
-    <Screen>
-      {books?.length ? (
-        <BookList
-          books={books}
-          renderBook={isbn => (
-            <BookItem isbn={isbn} action='delete' onPress={handlePress} />
-          )}
-        />
-      ) : (
-        <Text>Tu estantería está vacía.</Text>
-      )}
-    </Screen>
+    <GestureHandlerRootView style={{ flex: 1, backgroundColor: 'white' }}>
+      <BookList
+        books={books}
+        renderBook={isbn => (
+          <BookItem isbn={isbn} action='add' onPress={handlePress} />
+        )}
+        noBooksMsg={'Aún no tienes libros por leer.'}
+        bgImage={shelfImg}
+      />
+    </GestureHandlerRootView>
   )
 }
 
