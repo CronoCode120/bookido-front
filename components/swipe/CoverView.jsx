c033b564ecd56b60564c70ba9aae45689c227e76
CronoCode120 <ctaverafernandez@gmail.com>
2024-09-21 10:55:32 +0200

    removed BookInfo component from CoverView

--- components/swipe/CoverView.jsx
+++ components/swipe/CoverView.jsx
@@ -1,14 +1,9 @@
-import BookInfo from './BookInfo.jsx'
 import { CoverWrapper } from './styles/CoverView.js'
 import getCoverUri from '../../utils/getCoverUri.js'
 
-const CoverView = ({ title, author, publisher, isbn }) => {
+const CoverView = ({ title, isbn }) => {
   const cover = getCoverUri(isbn, 'L')
-  return (
-    <CoverWrapper src={cover} alt={'Portada de ' + title}>
-      <BookInfo {...{ title, author, publisher }} />
-    </CoverWrapper>
-  )
+  return <CoverWrapper src={cover} alt={'Portada de ' + title} />
 }
 
 export default CoverView
