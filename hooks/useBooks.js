3bd1aaf85ab05a0ed6621c5ea0efe0a02f6cb50a
CronoCode120 <ctaverafernandez@gmail.com>
2024-09-24 12:06:08 +0200

    implemented swipe up functionality

--- hooks/useBooks.js
+++ hooks/useBooks.js
@@ -5,11 +5,13 @@ import { getBooksData } from '../api/books.js'
 const useBooks = page => {
   const { session } = useSession()
   const [books, setBooks] = useState([])
+  const [doubtedBooks, setDoubtedBooks] = useState([])
 
   const sortBooks = fetchedBooks => {
-    if (!books.length) return setBooks(fetchedBooks)
+    if (!books.length) return setBooks(fetchedBooks.concat(doubtedBooks))
 
-    setBooks(prevBooks => prevBooks.concat(fetchedBooks))
+    setBooks(prevBooks => prevBooks.concat(fetchedBooks).concat(doubtedBooks))
+    setDoubtedBooks([])
   }
 
   useEffect(() => {
@@ -18,7 +20,10 @@ const useBooks = page => {
       .catch(err => console.log(err))
   }, [page])
 
-  return [books, setBooks]
+  const saveForLater = book =>
+    setDoubtedBooks(prevBooks => [...prevBooks, book])
+
+  return { books, saveForLater }
 }
 
 export default useBooks
