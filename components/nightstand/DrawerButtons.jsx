0b557eb3e3a0c7169d5924538d5a945ad97429db
CronoCode120 <ctaverafernandez@gmail.com>
2024-09-24 10:39:40 +0200

    adapted to new Button styles

--- components/nightstand/DrawerButtons.jsx
+++ components/nightstand/DrawerButtons.jsx
@@ -1,27 +1,11 @@
-import theme from '../../theme.js'
 import Button, { LinkButton } from '../Button.jsx'
-import { AddIcon } from '../icons'
-import {
-  ContentWrapper,
-  LabelWrapper,
-  LinkLabel,
-  LinkTitle,
-  Wrapper
-} from './styles/DrawerButtons.js'
+import { Wrapper } from './styles/DrawerButtons.js'
 
 const DrawerButtons = ({ isbn }) => {
   return (
     <Wrapper>
-      <LinkButton href={`/review/${isbn}`}>
-        <ContentWrapper>
-          <LabelWrapper>
-            <LinkTitle>Añadir</LinkTitle>
-            <LinkLabel>Reseña y valoración</LinkLabel>
-          </LabelWrapper>
-          <AddIcon color={theme.colors.WHITE} />
-        </ContentWrapper>
-      </LinkButton>
-      <Button type='tertiary'>Caja de donaciones</Button>
+      <LinkButton href={`/review/${isbn}`}>Marcar como leído</LinkButton>
+      <Button type='secondary'>Donar libro</Button>
     </Wrapper>
   )
 }
