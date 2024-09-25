6d3b626b25513a9e272552124f09f0158f8eab1c
CronoCode120 <ctaverafernandez@gmail.com>
2024-09-25 16:19:39 +0200

    refactored code

--- components/BookItem.jsx
+++ components/BookItem.jsx
@@ -16,9 +16,7 @@ import {
 import { Shadow } from 'react-native-shadow-2'
 
 const BookItem = ({ isbn, onPress, action }) => {
-  const book = useBookISBN(isbn, ['title', 'author', 'publisher'])
-
-  const { title, author } = book ?? {}
+  const { title, author } = useBookISBN(isbn, ['title', 'author']) ?? {}
   const authorInfo = getAuthors(author)
   const coverUri = getCoverUri(isbn)
   const [synopsis] = useBookSynopsis(isbn)
