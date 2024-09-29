a1f5da9d67589ae8850d2f8d0f8c744490c51979
CronoCode120 <ctaverafernandez@gmail.com>
2024-09-29 12:25:10 +0200

    styled progress bar, search input and 'next' button

--- pages/Onboarding.jsx
+++ pages/Onboarding.jsx
@@ -1,10 +1,29 @@
-import { Container, SearchInput, Title } from './styles/Onboarding'
+import SearchIcon from '../components/icons/SearchIcon.jsx'
+import {
+  Container,
+  SearchInput,
+  SearchWrapper,
+  Title,
+  StyledButton,
+  TopWrapper,
+  ProgressBar,
+  Progress
+} from './styles/Onboarding'
 
 const Onboarding = () => {
   return (
     <Container>
-      <Title>Dinos tus tres libros favoritos</Title>
-      <SearchInput />
+      <TopWrapper>
+        <ProgressBar>
+          <Progress />
+        </ProgressBar>
+        <Title>Dinos tus tres libros favoritos</Title>
+        <SearchWrapper>
+          <SearchInput />
+          <SearchIcon />
+        </SearchWrapper>
+      </TopWrapper>
+      <StyledButton>Siguiente</StyledButton>
     </Container>
   )
 }
--- pages/styles/Onboarding.js
+++ pages/styles/Onboarding.js
@@ -1,10 +1,35 @@
 import { SafeAreaView } from 'react-native-safe-area-context'
 import styled from 'styled-components/native'
+import Button from '../../components/Button.jsx'
 
 export const Container = styled(SafeAreaView)`
   flex: 1;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
+  padding-top: 24px;
+  padding-bottom: 40px;
+`
+
+export const TopWrapper = styled.View`
+  align-items: center;
+  justify-content: center;
+  width: 90%;
+  max-width: 340px;
+`
+
+export const ProgressBar = styled.View`
+  background: ${({ theme }) => theme.colors.NEUTRAL_200};
+  align-self: stretch;
+  height: 12px;
+  border-radius: 6px;
+  margin-bottom: 30px;
+`
+
+export const Progress = styled.View`
+  width: ${({ steps, progress }) => (100 / steps) * progress}%;
+  height: 100%;
+  border-radius: 6px;
+  background: ${({ theme }) => theme.colors.AMBER_600};
 `
 
 export const Title = styled.Text`
@@ -13,14 +38,29 @@ export const Title = styled.Text`
   text-align: center;
   margin-bottom: 30px;
 `
-export const SearchInput = styled.TextInput.attrs(({ theme }) => ({
-  placeholder: 'Buscar',
-  placeholderTextColor: theme.colors.NEUTRAL_500
-}))`
+
+export const SearchWrapper = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  gap: 5px;
   height: 40px;
-  width: 86%;
-  padding: 0 24px;
-  color: ${({ theme }) => theme.colors.NEUTRAL_500};
+  padding: 0 16px;
   border: 1px solid ${({ theme }) => theme.colors.NEUTRAL_300};
   border-radius: 30px;
+`
+
+export const SearchInput = styled.TextInput.attrs(({ theme }) => ({
+  placeholder: 'Buscar por título',
+  placeholderTextColor: theme.colors.NEUTRAL_500,
+  inputMode: 'search',
+  autoCorrect: false
+}))`
+  color: ${({ theme }) => theme.colors.NEUTRAL_500};
+  flex: 1;
+  height: 100%;
+`
+
+export const StyledButton = styled(Button)`
+  width: 80%;
 `
