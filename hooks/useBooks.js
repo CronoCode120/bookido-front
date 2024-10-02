55a82221e495f8f3fe1a85d0ca81a53b8f925c56
CronoCode120 <ctaverafernandez@gmail.com>
2024-10-02 14:08:07 +0200

    handled pages inside the 'useBooks' hook

--- hooks/useBooks.js
+++ hooks/useBooks.js
@@ -2,9 +2,10 @@ import { useState, useEffect } from 'react'
 import { useSession } from '../context/SessionProvider.js'
 import { getBooksData } from '../api/books.js'
 
-const useBooks = page => {
+const useBooks = curIdx => {
   const { session } = useSession()
   const [books, setBooks] = useState([])
+  const [page, setPage] = useState(1)
   const [doubtedBooks, setDoubtedBooks] = useState([])
 
   const sortBooks = fetchedBooks => {
@@ -13,6 +14,13 @@ const useBooks = page => {
     setBooks(prevBooks => prevBooks.concat(fetchedBooks).concat(doubtedBooks))
     setDoubtedBooks([])
   }
+
+  useEffect(() => {
+    if (!books) return
+    if (books.length - 3 <= curIdx) {
+      setPage(prevPage => prevPage + 1)
+    }
+  }, [curIdx])
 
   useEffect(() => {
     getBooksData(page, session)
