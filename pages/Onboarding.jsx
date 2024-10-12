da47816a07e39180d5ed4a3cadaca6660481156c
CronoCode120 <ctaverafernandez@gmail.com>
2024-10-12 10:45:47 +0200

    made suggestions pressable when TextInput is focused

--- pages/Onboarding.jsx
+++ pages/Onboarding.jsx
@@ -69,6 +69,7 @@ const Onboarding = () => {
                 alignSelf: 'stretch'
               }}
               ItemSeparatorComponent={Separator}
+              keyboardShouldPersistTaps='always'
             />
           </DropdownList>
         )}
