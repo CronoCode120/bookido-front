2ac8dbb6830b5b9d328e52d99baa362a9c9a1398
CronoCode120 <ctaverafernandez@gmail.com>
2024-10-15 16:23:21 +0200

    added 'cover' prop

--- components/swipe/CoverView.jsx
+++ components/swipe/CoverView.jsx
@@ -1,9 +1,9 @@
 import { CoverWrapper } from './styles/CoverView.js'
 import getCoverUri from '../../utils/getCoverUri.js'
 
-const CoverView = ({ title, isbn }) => {
-  const cover = getCoverUri(isbn, 'L')
-  return <CoverWrapper src={cover} alt={'Portada de ' + title} />
+const CoverView = ({ title, isbn, cover }) => {
+  const coverUri = cover ?? getCoverUri(isbn, 'L')
+  return <CoverWrapper src={coverUri} alt={'Portada de ' + title} />
 }
 
 export default CoverView
