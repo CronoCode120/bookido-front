de85ae8f4ec731a491a3cd86ff0a2de20281fc2f
CronoCode120 <ctaverafernandez@gmail.com>
2024-10-19 10:57:09 +0200

    cut title when it overflows

--- components/NavHeader.jsx
+++ components/NavHeader.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native'
 import BackButton from './BackButton.jsx'
+import { getWidthPercent } from '../utils/dimensions.js'
 
 const NavHeader = ({ title }) => {
   return (
@@ -12,10 +13,13 @@ const NavHeader = ({ title }) => {
 
 export default NavHeader
 
-const Title = styled.Text`
+const Title = styled.Text.attrs({
+  numberOfLines: 1
+})`
   font-size: 20px;
   font-weight: 400;
   font-family: ${({ theme }) => theme.fonts.HEADING};
+  max-width: ${getWidthPercent(85)};
 `
 
 const Wrapper = styled.View`
@@ -23,4 +27,5 @@ const Wrapper = styled.View`
   flex-direction: row;
   gap: 10px;
   padding: 8px 0;
+  width: 100%;
 `
