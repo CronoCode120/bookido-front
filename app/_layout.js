2e7c08e9380ba066cc64daba67ca15b213f71d31
CronoCode120 <ctaverafernandez@gmail.com>
2024-10-24 11:46:30 +0200

    applied white background to the entire app

--- app/_layout.js
+++ app/_layout.js
@@ -4,22 +4,25 @@ import { Stack } from 'expo-router'
 
 import theme from '../theme.js'
 import { StatusBar } from 'expo-status-bar'
+import { View } from 'react-native'
 
 const RootLayout = () => {
   return (
     <SessionProvider>
       <ThemeProvider theme={theme}>
-        <StatusBar backgroundColor='white' style='dark' />
-        <Stack
-          screenOptions={{
-            headerTitle: '',
-            headerShadowVisible: false,
-            headerStyle: { backgroundColor: 'transparent' },
-            headerShown: false,
-            contentStyle: { backgroundColor: 'transparent' },
-            animation: 'slide_from_right'
-          }}
-        />
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+          <StatusBar backgroundColor='white' style='dark' />
+          <Stack
+            screenOptions={{
+              headerTitle: '',
+              headerShadowVisible: false,
+              headerStyle: { backgroundColor: 'transparent' },
+              headerShown: false,
+              contentStyle: { backgroundColor: 'transparent' },
+              animation: 'slide_from_right'
+            }}
+          />
+        </View>
       </ThemeProvider>
     </SessionProvider>
   )
